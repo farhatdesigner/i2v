4a7f97e550e7a47c35797db9eac1b3ed321822d5
unknown <farhat.ullah@repindia.com>
2025-10-27 17:30:27 +0530

    update home page code 2

--- wp-content/themes/repindia/our-product.php
+++ wp-content/themes/repindia/our-product.php
@@ -6,172 +6,19 @@
 
 
 
-<section class="hero-slider hero-style">
-   <div class="swiper-container">
-      <div class="swiper-wrapper">
-         <div class="swiper-slide">
-            <div class="slide-inner slide-bg-image" data-background="<?php echo get_template_directory_uri(); ?>/assets/images/bannerslider-1.png">
-               <div class="custom-container">
-                  <div data-swiper-parallax="300" class="slide-title">
-                     <h2>i2V Systems – Driving the future of smart, secure, and scalable video surveillance</h2>
-                  </div>
-                  <div data-swiper-parallax="400" class="slide-text">
-                     <p>AI-powered video management, analytics, and security solutions tailored for enterprises, public sector agencies, and critical infrastructure.</p>
-                  </div>
-                  <div class="clearfix"></div>
-                  <div data-swiper-parallax="500" class="slide-btns">
-                     <a href="#" class="theme-btn  xl-btn">Request a demo</a>
-                     <a href="#" class="theme-btn xl-btn grey-btn"> See how i2V works</a>
-                  </div>
-               </div>
-            </div>
-            <!-- end slide-inner -->
-         </div>
-         <!-- end swiper-slide -->
-
-         <div class="swiper-slide">
-            <div class="slide-inner slide-bg-image" data-background="<?php echo get_template_directory_uri(); ?>/assets/images/bannerslider-2.png">
-               <div class="custom-container">
-                  <div data-swiper-parallax="300" class="slide-title">
-                     <h2>A smarter way to secure what matters—reliable, scalable, and ready for the future</h2>
-                  </div>
-                  <div data-swiper-parallax="400" class="slide-text">
-                     <p>Purpose-built for high-stakes environments, our platform provides real-time insights and total operational visibility.</p>
-                  </div>
-                  <div class="clearfix"></div>
-                  <div data-swiper-parallax="500" class="slide-btns">
-                     <a href="#" class="theme-btn  xl-btn">Request a demo</a>
-                     <a href="#" class="theme-btn xl-btn grey-btn"> See how i2V works</a>
-                  </div>
-               </div>
-            </div>
-            <!-- end slide-inner -->
-         </div>
-         <!-- end swiper-slide -->
-
-
-         <div class="swiper-slide">
-            <div class="slide-inner slide-bg-image" data-background="<?php echo get_template_directory_uri(); ?>/assets/images/bannerslider-3.png">
-               <div class="custom-container">
-                  <div data-swiper-parallax="300" class="slide-title">
-                     <h2>End-to-end video security built for every industry</h2>
-                  </div>
-                  <div data-swiper-parallax="400" class="slide-text">
-                     <p>From intelligent VMS to advanced AI analytics, i2V offers flexible, high-performance solutions for diverse sectors and use cases.</p>
-                  </div>
-                  <div class="clearfix"></div>
-                  <div data-swiper-parallax="500" class="slide-btns">
-                     <a href="#" class="theme-btn  xl-btn">Request a demo</a>
-                     <a href="#" class="theme-btn xl-btn grey-btn"> See how i2V works</a>
-                  </div>
-               </div>
-            </div>
-            <!-- end slide-inner -->
-         </div>
-         <!-- end swiper-slide -->
-
-
-         <div class="swiper-slide">
-            <div class="slide-inner slide-bg-image" data-background="<?php echo get_template_directory_uri(); ?>/assets/images/bannerslider-4.png">
-               <div class="custom-container">
-                  <div data-swiper-parallax="300" class="slide-title">
-                     <h2>Trusted AI video surveillance for government & enterprise</h2>
-                  </div>
-                  <div data-swiper-parallax="400" class="slide-text">
-                     <p>Secure, intelligent, and built for scale—i2V delivers mission-ready surveillance solutions for public and enterprise operations.</p>
-                  </div>
-                  <div class="clearfix"></div>
-                  <div data-swiper-parallax="500" class="slide-btns">
-                     <a href="#" class="theme-btn  xl-btn">Request a demo</a>
-                     <a href="#" class="theme-btn xl-btn grey-btn"> See how i2V works</a>
-                  </div>
-               </div>
-            </div>
-            <!-- end slide-inner -->
-         </div>
-         <!-- end swiper-slide -->
-      </div>
-      <!-- end swiper-wrapper -->
-
-      <!-- swipper controls -->
-      <div class="swiper-pagination"></div>
-      <div class="custom-container position-relative">
-         <div class="swiper-button-container position-absolute">
-            <div class="swiper-button-next">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/right-arrow.svg" alt="Next">
-            </div>
-            <div class="swiper-button-prev">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/left-arrow.svg" alt="Prev">
-            </div>
-         </div>
-      </div>
-   </div>
-</section>
-<!-- end of hero slider -->
 
 
 
-<section class="microspace-custom_inside">
-   <div class="custom-container">
-      <div class="brand_heading_color text-center">
-         <h5>Trusted by leading global brands for intelligent, integrated security. <small><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/tertiary.svg" alt="tertiary"></small>
-         </h5>
-      </div>
-   </div>
-
-
-
-   <div class="w-full text-center mt-4">
-      <div class="swiper brandslider">
-         <div class="swiper-wrapper">
-            <div class="swiper-slide">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/brand-logo_1.svg" alt="brand">
-            </div>
-            <div class="swiper-slide">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/brand-logo_2.svg" alt="brand">
-            </div>
-            <div class="swiper-slide">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/brand-logo_3.svg" alt="brand">
-            </div>
-            <div class="swiper-slide">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/brand-logo_4.svg" alt="brand">
-            </div>
-            <div class="swiper-slide">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/brand-logo_5.svg" alt="brand">
-            </div>
-            <div class="swiper-slide">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/brand-logo_6.svg" alt="brand">
-            </div>
-            <div class="swiper-slide">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/brand-logo_7.svg" alt="brand">
-            </div>
-            <div class="swiper-slide">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/brand-logo_8.svg" alt="brand">
-            </div>
-            <div class="swiper-slide">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/brand-logo_9.svg" alt="brand">
-            </div>
-            <div class="swiper-slide">
-               <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/brand-logo_4.svg" alt="brand">
-            </div>
-         </div>
-         <div class="swiper-pagination"></div>
-      </div>
-   </div>
-</section>
-
-
-
-<section class="microspace-inside bg-white">
+<!-- <section class="microspace-inside bg-white">
    <div class="custom-container mx-auto">
 
 
-            <div class="text-left">
-               <h2 class="uppercase mb-8 lg:mb-16 quote">
-                  Integrated solutions to secure and streamline every facet of your operations
-               </h2>
-            </div>
-   
+      <div class="text-left">
+         <h2 class="uppercase mb-8 lg:mb-16 quote">
+            Integrated solutions to secure and streamline every facet of your operations
+         </h2>
+      </div>
+
 
 
 
@@ -226,7 +73,7 @@
 
 
    </div>
-</section>
+</section> -->
 
 
 
