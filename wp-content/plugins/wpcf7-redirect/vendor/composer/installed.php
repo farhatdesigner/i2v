8c87ca1a86d9f245dd06ac033d7365f72706fbec
User <farhat.ullah@repindia.com>
2025-12-19 13:06:54 +0530

    animation and update file

---
+++ wp-content/plugins/wpcf7-redirect/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/redirection-for-contact-form-7',
+        'pretty_version' => 'v3.2.7',
+        'version' => '3.2.7.0',
+        'reference' => '9cb057ba7f84897e50999bb7ad33ed26754d1d0b',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/redirection-for-contact-form-7' => array(
+            'pretty_version' => 'v3.2.7',
+            'version' => '3.2.7.0',
+            'reference' => '9cb057ba7f84897e50999bb7ad33ed26754d1d0b',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.49',
+            'version' => '3.3.49.0',
+            'reference' => '605f78bbbd8526f7597a89077791043d9ecc8c20',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
