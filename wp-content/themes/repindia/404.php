0b7890385bcf5d829ae80b6348f4884ee73c099f
Admin <ravindra.verma@repindia.com>
2026-01-13 18:11:37 +0530

    404 functionality

--- wp-content/themes/repindia/404.php
+++ wp-content/themes/repindia/404.php
@@ -1,22 +1,26 @@
 <?php 
 /**
- * The template for displaying 404 pages (not found).
+ * Template Name: 404 Page
  *
  * @package repindia
  */
 repindia_get_header(); 
 global $repindia_option;	?>
-    <div class="error-area page-404">
-        <div class="container">
-            <div class="error-content">
-                    <h1 class="error-title"><?php echo esc_html( "404", 'repindia' ); ?></h1>
-                    <h2><?php echo esc_html( "Page Not Found", 'repindia' ); ?></h2>
-					<div class="error_bitton">
-						<a href="<?php echo esc_url(get_home_url('/')); ?>" class="button_404"><?php echo esc_html('Back To Home', 'repindia');?></a>
-                    </div>		
-            </div>
-        </div>
-    </div>
-	<?php	
-	get_footer(); 
-	?>
+<?php get_header(); ?>
+
+<?php
+$page = get_page_by_path('error-404'); // your page slug
+
+if ($page) {
+    echo \Elementor\Plugin::instance()
+        ->frontend
+        ->get_builder_content_for_display($page->ID);
+} else {
+    echo '<h1>404 - Page Not Found</h1>';
+}
+?>
+
+<?php get_footer(); ?>
+
+
+
