c698b12cbd628af032d50484130104b8389c98b6
Admin <ravindra.verma@repindia.com>
2026-01-27 17:57:54 +0530

    responsive

--- wp-content/themes/repindia/inc/elementor-addons/widgets/custom_tooltip.php
+++ wp-content/themes/repindia/inc/elementor-addons/widgets/custom_tooltip.php
@@ -918,7 +918,7 @@ class Custom_Tooltip extends Widget_Base
     /* button.ctw-learn-more-btn{ box-shadow: none!important;border: none;border-bottom: 1px solid rgba(255, 255, 255, 0.20); } */
     .ctw-title .ctw-text p{ font-size: 14px;font-weight: 500!important;line-height: 26px;color: #5C5C5C; }
     .tooltiptitlebox .ctw-title .ctw-text p{ color: #262A30;font-size: 20px; }
-    .tooltiptitlebox.toptooltitle .ctw-title .ctw-text p{ font-size: 14px; }
+    .toptooltitle .ctw-title .ctw-text p{ font-size: 14px;color: #262A30; }
     .para_tooltip .ctw-title .ctw-text p{ font-size: 16px; }
     .ctw-title:hover .border-b{ border-bottom: 2px solid #9ea1a8!important; }
     .js-dark .ctw-title:hover .border-b { border-bottom: 2px solid #7d8895 !important; }
