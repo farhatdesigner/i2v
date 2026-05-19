ee40df694dcc53a7a4f9f66df867c9053035f552
Ravindra <ravindra.verma@repindia.com>
2026-05-19 05:51:42 +0000

    Smart city tabbed wiget fixes

--- wp-content/themes/repindia/inc/elementor-addons/widgets/tabbed_custom_swiper.php
+++ wp-content/themes/repindia/inc/elementor-addons/widgets/tabbed_custom_swiper.php
@@ -503,7 +503,7 @@ class Tabbed_Custom_Swiper extends Widget_Base
                         <div class="swiper-button-prev">
                             <?php //if ($tab_index == 1): ?>
                                 <!-- <img src="<?php //echo esc_url(home_url('/')); ?>wp-content/themes/repindia/assets/images/icons/arrow-white.svg" alt="Prev"> -->
-                            <?php// else: ?>
+                            <?php //else: ?>
                                 <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M20 0.5C30.7696 0.5 39.5 9.23045 39.5 20C39.5 30.7696 30.7696 39.5 20 39.5C9.23045 39.5 0.5 30.7696 0.5 20C0.5 9.23045 9.23045 0.5 20 0.5Z"
